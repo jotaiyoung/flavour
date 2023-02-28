b567e0e6d31ee947d290df16fe815fa53c5d716a
jotaiyoung <45022965+jotaiyoung@users.noreply.github.com>
2023-02-28 13:21:23 +0900

    a
    a

---
+++ resources/views/index.blade.php
@@ -0,0 +1,400 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>flavour712x</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+ 
+</head>
+
+<body>
+
+  <!-- ======= Top Bar ======= -->
+  <section id="topbar" class="d-flex align-items-center">
+    <div class="container d-flex justify-content-center justify-content-md-between">     
+      <div class="social-links d-none d-md-block">
+        
+        <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
+      </div>
+    </div>
+  </section>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="d-flex align-items-center">
+    <div class="container d-flex align-items-center">
+
+      <h1 class="logo me-auto"><a href="index.html">FLAVOUR712</a></h1>
+      <!-- Uncomment below if you prefer to use an image logo -->
+      <!-- <a href="index.html" class="logo me-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+
+      <nav id="navbar" class="navbar">
+      	<div id="unline"></div>
+        <ul>          
+          <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
+          <li class="dropdown"><a href="#"><span>블렝딩 과일차종류</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+              <li><a href="#">기브미 허그</a></li>              
+              <li><a href="#">엑스트라 오디너리데이</a></li>
+              <li><a href="#">버터플라이가든</a></li>
+              <li><a href="#">버터 앤 스위트메모리</a></li>
+              <li><a href="#">선물세트 4종제품</a></li>
+            </ul>
+          </li>
+          <li><a class="nav-link scrollto " href="#services">맛있게 마시는방법</a></li>
+          <li><a class="nav-link scrollto " href="#info">제품 상세 정보</a></li>
+          <li><a class="nav-link scrollto " href="#portfolio">문의하기</a></li>      
+          
+          
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero">
+    <div id="heroCarousel" data-bs-interval="5000" class="carousel slide carousel-fade" data-bs-ride="carousel">
+
+      <ol class="carousel-indicators" id="hero-carousel-indicators"></ol>
+
+      <div class="carousel-inner" role="listbox">
+
+        <!-- Slide 1 -->
+        <div class="carousel-item active" style="background-image: url(assets/img/slide/slide-1.png)">
+          <div class="carousel-container">
+            <div class="container">
+              <h2 class="animate__animated animate__fadeInDown">Welcome to <span>FLAVOUR712</span></h2>
+              <p class="animate__animated animate__fadeInUp">지금가지 경험해 보지 못한 매혹적인 맛과 향으로 당신의 감각을 자극합니다</p>
+              <!-- <a href="#about" class="btn-get-started animate__animated animate__fadeInUp scrollto">Read More</a> -->
+            </div>
+          </div>
+        </div>
+
+        <!-- Slide 2 -->
+        <div class="carousel-item" style="background-image: url(assets/img/slide/123645.jpg)">
+          <div class="carousel-container">
+            <div class="container">
+              <h2 class="animate__animated animate__fadeInDown">다양한 과일의 풍부한 맛과 향을 담은 </h2>
+              <p class="animate__animated animate__fadeInUp">언제 어디서든 편안하게 즐길 수 있는 블렌딩 과일차</p>
+              <!-- <a href="#about" class="btn-get-started animate__animated animate__fadeInUp scrollto">Read More</a> -->
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+      <a class="carousel-control-prev" href="#heroCarousel" role="button" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon bi bi-chevron-left" aria-hidden="true"></span>
+      </a>
+
+      <a class="carousel-control-next" href="#heroCarousel" role="button" data-bs-slide="next">
+        <span class="carousel-control-next-icon bi bi-chevron-right" aria-hidden="true"></span>
+      </a>
+
+    </div>
+  </section><!-- End Hero -->
+
+  <main id="main">
+
+  
+
+    <!-- ======= About Us Section ======= -->
+    <section id="about" class="about">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>About Us</h2>          
+        </div>
+
+        <div class="row">
+          <div class="col-lg-6 order-1 order-lg-2">
+            <img src="image/1.jpg" class="img-fluid" alt="">
+          </div>
+          <div class="col-lg-6 pt-4 pt-lg-0 order-2 order-lg-1 content">
+            <h3>flavour 712의 과일베이스 블렌딩 티는</h3>
+            <p class="fst-italic">
+              지금가지 경험해 보지 못한 매혹적인 맛과 향으로 당신의 감각을 자극합니다 </p>
+            <ul>
+              <li><i class="bi bi-check-circled"></i> 늦은 시간에도 즐길 수 있는 카페인프리.</li>
+              <li><i class="bi bi-check-circled"></i> 건강을 위해 충분한 물 섭취를 도와줄 수 있는 차.</li>
+              <li><i class="bi bi-check-circled"></i> 풍부한 맋과 향을 담은 아로마 테라피 음료.</li>
+            </ul>            
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End About Us Section -->
+
+   
+
+    <!-- ======= Our Clients Section ======= -->
+    <section id="clients" class="clients">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>블렝딩 과일차종류</h2>
+          <p>다양한 종류의 차를 즐겨 보세요</p>
+        </div>
+
+        <div class="clients-slider swiper">
+          <div class="swiper-wrapper align-items-center">
+            <div class="swiper-slide"><img src="image/BASM.jpg" class="img-fluid" alt=""></div>
+            <div class="swiper-slide"><img src="image/BG.jpg" class="img-fluid" alt=""></div>
+            <div class="swiper-slide"><img src="image/EOD.jpg" class="img-fluid" alt=""></div>
+            <div class="swiper-slide"><img src="image/GH.jpg" class="img-fluid" alt=""></div>            
+          </div>
+          <div class="swiper-pagination"></div>
+        </div>
+
+      </div>
+    </section><!-- End Our Clients Section -->
+
+    <!-- ======= Services Section ======= -->
+    <section id="services" class="services">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>음용 방법</h2>
+          <p>flavour712는 계절에 따라 따뜻하게 또는 차갑네 함께 즐기는 음식에 따라, 다양하게 즐길 수 있습니다.</p>
+        </div>
+        <div class="make"><img src="image/135953.jpg" style="width:1000px; height:700px;" class="img-fluid" alt=""></div>
+                
+      </div>
+    </section><!-- End Services Section -->
+
+ 
+    
+    <
+
+    
+    <section id="info" class="info section-bg">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>제품 상세 정보 </h2>
+         
+        </div>
+				<table class="tg">
+					<thead>
+						<tr>
+							<th class="tg-xbtn">제품명</th>
+							<th class="tg-xbtn">버터플라이 가든</th>
+							<th class="tg-xbtn">기브미어 허그</th>
+							<th class="tg-xbtn">엑스트라오디너리데이</th>
+							<th class="tg-xbtn">버터앤 스위트메모리</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td class="tg-xbtn">식품유형</td>
+							<td class="tg-7btt" colspan="4">침출차</td>
+						</tr>
+						<tr>
+							<td class="tg-xbtn">품목보고번호</td>
+							<td class="tg-7btt">20140086585562</td>
+							<td class="tg-7btt">20140086585565</td>
+							<td class="tg-7btt">20140086585563</td>
+							<td class="tg-7btt">20140086585564</td>
+						</tr>
+						<tr>
+							<td class="tg-xbtn">내용량</td>
+							<td class="tg-7btt" colspan="4">40g (4g x 10EA)</td>
+						</tr>
+						<tr>
+							<td class="tg-xbtn">원재료명 <br>및<br>함량
+							</td>
+							<td class="tg-7btt">파인애플큐브 36%<br>망고큐브 29.5% <br>파인애플 8%<br>망고
+								8%, 오렌지 8%<br>동결건조감귤 4%, 홍화잎 3%<br>동결건조딸기 2%, 매리골드 2%<br>합성향료(망고)
+							</td>
+							<td class="tg-7btt">사과 60%, 아몬드 26%<br>시나몬 9%, 비트루트 4.5%<br>합성향료(아몬드)
+							</td>
+							<td class="tg-7btt">사과 38%, 히비스커스 24.5%<br>로즈힙 20%, 파인애플큐브 10%<br>계피
+								5%, 해바라기꽃잎 2%<br>합성향료(배향)
+							</td>
+							<td class="tg-7btt">사과 67%, 히비스커스 9%<br>로즈힙 7.5%, 생강 5%, <br>후추
+								5%, 멜론큐브 4%<br>장비꽃잎 1%, 당귤열매껍질 1%<br>합성향료(복숭아)
+							</td>
+						</tr>
+						<tr>
+							<td class="tg-xbtn">포장재질</td>
+							<td class="tg-7btt" colspan="4">여과지-PLA / 개별포장지-폴리에틸렌 / 외부케이스 : 종</td>
+						</tr>
+						<tr>
+							<td class="tg-xbtn">원산지</td>
+							<td class="tg-7btt" colspan="4">독일</td>
+						</tr>
+						<tr>
+							<td class="tg-xbtn">유통기한</td>
+							<td class="tg-7btt" colspan="4">별도표기일까지</td>
+						</tr>
+						<tr>
+							<td class="tg-xbtn">제조원</td>
+							<td class="tg-7btt" colspan="4">(주)에스엠팜 / 경기도 파주시 장면산길 56-22</td>
+						</tr>
+						<tr>
+							<td class="tg-xbtn">판매원</td>
+							<td class="tg-7btt" colspan="4">티지트레이드 / 경기도 파주시 가람로 116번지 107 832호</td>
+						</tr>
+						<tr>
+							<td class="tg-xbtn">소비자상담실</td>
+							<td class="tg-7btt" colspan="4">070-4193-2000 (수신자 부담)</td>
+						</tr>
+						<tr>
+							<td class="tg-xbtn">반품 및 교환</td>
+							<td class="tg-7btt" colspan="4">판매처 및 구입처</td>
+						</tr>
+						<tr>
+							<td class="tg-epna">보관방법</td>
+							<td class="tg-7btt" colspan="4">직산광선을 피하고 서늘한 실온에 보관하세요</td>
+						</tr>
+						<tr>
+							<td class="tg-epna">주의사항</td>
+							<td class="tg-7btt" colspan="4">본 제품은 메밀, 땅콩, 대구, 밀, 새우, 조개류를 사용한
+								제품과 같은 시설에서 생산하고 있습니다</td>
+						</tr>
+						<tr>
+    						<td class="tg-km2t" colspan="5">*유통과정 중 제품에 이상이 있을경우에는 공정거래위원회 고시 소비자 분쟁 해결기준에 의거 교롼 또는 보상을 받을수 있습니다<br>*부정불량식품 신고는 국번없이 1399</td>
+  						</tr>
+					</tbody>					
+				</table>
+				
+					
+			</div>
+    </section><!-- End Team Section -->
+
+    <!-- ======= Contact Section ======= -->
+    <section id="contact" class="contact">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>오시는길</h2>
+          <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
+        </div>
+
+        <div class="row">
+
+          <div class="col-lg-5 d-flex align-items-stretch">
+            <div class="info">
+              <div class="address">
+                <i class="bi bi-geo-alt"></i>
+                <h4>Location:</h4>
+                <p>A108 Adam Street, New York, NY 535022</p>
+              </div>
+
+              <div class="email">
+                <i class="bi bi-envelope"></i>
+                <h4>Email:</h4>
+                <p>info@example.com</p>
+              </div>
+
+              <div class="phone">
+                <i class="bi bi-phone"></i>
+                <h4>Call:</h4>
+                <p>+1 5589 55488 55s</p>
+              </div>
+
+              <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621" frameborder="0" style="border:0; width: 100%; height: 290px;" allowfullscreen></iframe>
+            </div>
+
+          </div>
+
+          <div class="col-lg-7 mt-5 mt-lg-0 d-flex align-items-stretch">
+            <form action="forms/contact.php" method="post" role="form" class="php-email-form">
+              <div class="row">
+                <div class="form-group col-md-6">
+                  <label for="name">Your Name</label>
+                  <input type="text" name="name" class="form-control" id="name" required>
+                </div>
+                <div class="form-group col-md-6 mt-3 mt-md-0">
+                  <label for="name">Your Email</label>
+                  <input type="email" class="form-control" name="email" id="email" required>
+                </div>
+              </div>
+              <div class="form-group mt-3">
+                <label for="name">Subject</label>
+                <input type="text" class="form-control" name="subject" id="subject" required>
+              </div>
+              <div class="form-group mt-3">
+                <label for="name">Message</label>
+                <textarea class="form-control" name="message" rows="10" required></textarea>
+              </div>
+              <div class="my-3">
+                <div class="loading">Loading</div>
+                <div class="error-message"></div>
+                <div class="sent-message">Your message has been sent. Thank you!</div>
+              </div>
+              <div class="text-center"><button type="submit">Send Message</button></div>
+            </form>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Contact Section -->
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+    <div class="container">
+      <h3>Green</h3>
+      <p>Et aut eum quis fuga eos sunt ipsa nihil. Labore corporis magni eligendi fuga maxime saepe commodi placeat.</p>
+      <div class="social-links">
+        <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+        <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+        <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+        <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
+        <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+      </div>
+      <div class="copyright">
+        &copy; Copyright <strong><span>Green</span></strong>. All Rights Reserved
+      </div>
+      <div class="credits">
+        <!-- All the links in the footer should remain intact. -->
+        <!-- You can delete the links only if you purchased the pro version. -->
+        <!-- Licensing information: https://bootstrapmade.com/license/ -->
+        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/green-free-one-page-bootstrap-template/ -->
+        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+      </div>
+    </div>
+  </footer><!-- End Footer -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
